a0c8e11a6c5ca3996977f37898d4129fe88a5e1a
FeliciaPompei <f.pompei@hotmail.com>
2022-06-13 20:13:18 +0200

    created sponsorship seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            ServiceSeeder::class
+            ServiceSeeder::class,
+            SponsorshipSeeder::class,
         ]);
     }
 }
---
+++ database/seeds/SponsorshipSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Sponsorship;
+class SponsorshipSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $sponsorships = [
+            [
+                'name' => 'Bronze Sponsor',
+                'period' => '24 ore',
+                'price' => 9.99
+            ],
+            [
+                'name' => 'Silver Sponsor',
+                'period' => '72 ore',
+                'price' => 5.99
+            ],
+            [
+                'name' => 'Gold Sponsor',
+                'period' => '144 ore',
+                'price' => 9.99
+            ],
+        ];
+
+        for ($i=0; $i < count($sponsorships) ; $i++) {
+            $newSponsorship = new Sponsorship;
+            $newSponsorship->name = $sponsorships[$i]['name'];
+            $newSponsorship->period = $sponsorships[$i]['period'];
+            $newSponsorship->price = $sponsorships[$i]['price'];
+            $newSponsorship->save();
+        }
+    }
+}
