7a39bffb328ac131881376807e6a0b66375e47e6
FeliciaPompei <f.pompei@hotmail.com>
2022-06-14 09:47:36 +0200

    added layout for edit and create

---
+++ resources/views/layouts/creatPage.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    @include('layouts.partials.head')
+    <body>
+        <div class="container">
+            <div class="row justify-content-center">
+                @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ( $errors->all() as $error )
+                            <li>
+                                {{$error}}
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+
+                <div class="col-6 py-3">
+                    <form class="text-center"
+                    action= @yield('route')
+                    method="POST"
+                    enctype="multipart/form-data">
+                        @csrf
+                        @yield('form-content')
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
