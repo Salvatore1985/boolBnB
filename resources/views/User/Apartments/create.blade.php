4623307f87aec8f1e156f5eef7d5ec614d0cbbfa
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-06-14 14:37:35 +0200

    add form city house_number

---
+++ resources/views/User/Apartments/create.blade.php
@@ -0,0 +1,142 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="wrapper w-75 mx-auto">
+        <div class="container-fluid">
+            <div class="row p-4">
+                {{-- @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif --}}
+            </div>
+            <div class="row p-4 d-flex ">
+
+                {{-- ? Per ogni valore rilevante ai fini del salvataggio abbiamo creato: --}}
+                {{-- # Un campo modificabile (tipicamente un input) in cui l'utente può inserire i contenuti --}}
+                {{-- § Un attributo del campo relativo di input chiamato "name" che ci consente di recupare il valore dal controller --}}
+                <div class="bg-primary text-white rounded p-4 col-6 justify-content-center">
+                    <form action="{{ route('user.apartments.store') }}" method="post">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="title" class="form-label">Inserisci il titolo dell' appartamento</label>
+                            <input type="text" class="form-control" id="title" name="title" id="title">
+                            @error('content')
+                                <div class="alert alert-danger">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="address">Inserisci la via</label>
+                            <input type="text" name="address" id="address">
+                            @error('title')
+                                <div class="alert alert-danger">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="house_number">Inserisci il numero civico</label>
+                            <input type="text" name="house_number" id="house_number">
+                            @error('title')
+                                <div class="alert alert-danger">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="city">Inserisci la città</label>
+                            <input type="text" name="city" id="city">
+                            @error('title')
+                                <div class="alert alert-danger">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="n_rooms">Inserisci il numero delle stanze</label>
+                            <input type="text" name="n_rooms" id="n_rooms">
+                            @error('image_url')
+                                <div class="alert alert-danger">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+
+                            <label for="description" class="form-label">Inserisci la descrizione
+                                dell'appartamento</label>
+                            <textarea class="form-control" id="description" name="description" id="description"></textarea>
+                            @error('content')
+                                <div class="alert alert-danger">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="sqr_meters" class="form-label">Inserisci i metri dell'appartamento</label>
+                            <input type="text" class="form-control" id="sqr_meters" name="sqr_meters" id="sqr_meters">
+                            @error('content')
+                                <div class="alert alert-danger">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="n_beds" class="form-label">Inserisci numero dei letti</label>
+                            <input type="text" class="form-control" id="n_beds" name="n_beds" id="n_beds">
+                            @error('content')
+                                <div class="alert alert-danger">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="n_bathrooms" class="form-label">Inserisci il numero dei bagni</label>
+                            <input type="text" class="form-control" id="n_bathrooms" name="n_bathrooms" id="n_bathrooms">
+                            @error('content')
+                                <div class="alert alert-danger">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="n_floor" class="form-label">Inserisci il numero dei piani</label>
+                            <input type="text" class="form-control" id="n_floor" name="n_floor" id="n_floor">
+                            @error('content')
+                                <div class="alert alert-danger">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="price" class="form-label">Inserisci il prezzo</label>
+                            <input type="text" class="form-control" id="price" name="price" id="price">
+                            @error('content')
+                                <div class="alert alert-danger">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <button type="submit" class="btn btn-primary">Pubblica il tuo appartamento</button>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+@endsection
