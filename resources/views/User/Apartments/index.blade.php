c6dc676c2cf9b0f31f442ff67485011cce6417d3
FeliciaPompei <f.pompei@hotmail.com>
2022-06-20 09:12:15 +0200

    image foreach

--- resources/views/User/Apartments/index.blade.php
+++ resources/views/User/Apartments/index.blade.php
@@ -29,17 +29,17 @@
                 <div class="col-6 ">
                     <div class="card-deck">
                         <div class="card mb-5">
-                            {{-- @foreach ($apartment->images as $image)
+                            @foreach ($apartment->images as $image)
                                 @if (str_starts_with($image->link, 'https://') || str_starts_with($image->link, 'http://'))
                                     <img class="card-img-top img-apartment" src="{{ $image->link }}"
                                         alt="{{ $apartment->title }}">
-                                @else
+                                @elseif (!str_starts_with($image->link, 'https://') || !str_starts_with($image->link, 'http://'))
                                     <img class="card-img-top img-apartment"
                                         src="{{ asset('/storage') . '/' . $image->link }}"
                                         alt="{{ $apartment->title }}">
                                 @endif
-                            @endforeach --}}
-                            @if (property_exists('link', $apartment->images))
+                            @endforeach
+                            {{-- @if (property_exists('link', $apartment->images))
                                 @if (str_starts_with($apartment->images[0]->link, 'https://') || str_starts_with($apartment->images[0]->link, 'http://'))
                                     <img class="card-img-top img-apartment" src="{{ $apartment->images[0]->link }}"
                                         alt="{{ $apartment->title }}">
@@ -50,7 +50,7 @@
                                 @endif
                                 @else
                                     <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAMFBMVEXh6vH////5+vrl7fPv8/f0+fzt9Pbf6vD+/f/j6/Lf6vL///3f6fLr8fbv8/by9vm0HxD7AAACbUlEQVR4nO3c65abIBRAYVHTDALx/d+2VBuDII6zLHro2t/fmEn2HETn2jQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADkG29irNHa4I9G9+qu83B2BTauu01JIIYUUUkghhbcX/ipFTGH3LKOTUmi6Qq/QGQrLovA8Ckuj8LzqC/98A2ZX5YXd2Lajf6beOaTmwm5+tnnsBNZc+NTt/OaNanWT/U5hzYWjWjzyh1VSqK1ONpTPW/fyz66kcHCqixJtHwSqPvvUKgq1D/Rjik61MZzhWPd5aJ3fTfwR6yn+P4XzBFU8RevCwvzPJCoo9IFmepdmPUUdFuqaZzgt0fcUw8T+88Ar/wrSC/UywemgcKE+7Tg/ZNRY8V2bDScYT9G6r+mdu+fOK8guXE8wmaK/Vet6980PPmUXDvEEVXLRCL942lwGkguny0RSmF76//ILuh/SE1JyYXIObk/xzfn17NJH5BZ+LvSHpmjno10yRbmFySYTDHFjivONnZ9iHC+3MLNE31NcJy7zNsn9m9DC9DIRTXG9UD977jTF1UIVWpjbZDJTDM/YeLsRWfjdBKMprj8d8RRFFm5d6Dca31OM99z1diOwMHehT01TTBe0WV00BBYem+AyRbdxcDhFgYX6YN80RZu5LTCSZ9gcLzSqz+xIZvloAgt/MsN8u+QZUkghhRRSSCGFFFJIIYWXFKqv85Towr3fcDpKSy78xygsjcLzKCyNwvPEFCpthxKsVlIKXVeGE1NYHoUUUkghhRRSeFfh2D6u0o53BOpmuOz/RA17f8MHAAAAAAAAAAAAAAAAAAAAAAAAAAAAALjcb3yLQG5tF3tgAAAAAElFTkSuQmCC" alt="no image">
-                            @endif
+                            @endif --}}
                             {{-- @dump($apartment->images->link[0]) --}}
                             <div class="card-body shadow rounded p-4">
                                 <h1 class="card-title  text-muted ">{{ $apartment->title }}</h1>
