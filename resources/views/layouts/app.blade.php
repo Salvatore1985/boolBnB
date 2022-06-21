d3ef05e4ea768191d192a79b51d44b0bcb1b047b
FeliciaPompei <f.pompei@hotmail.com>
2022-06-21 18:23:27 +0200

    debug, removed header link for the moment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,27 +28,30 @@
                                     <a class="nav-link" href="{{ route('user.apartments.index') }}">I tuoi
                                         Appartmenti</a>
                                 </li>
-                                <li class="nav-item" @if (Auth::user()->id == 1) @endif>
-                                    <a class="nav-link" href="{{ route('admin.services.index') }}">
-                                        Servizi
-                                    </a>
-                                </li>
-                                <li class="nav-item" @if (Auth::user()->id == 1) @endif>
-                                    <a class="nav-link" href="{{ route('user.users.index') }}">
-                                        Utenti
-                                    </a>
-                                </li>
-                                <li class="nav-item" @if (Auth::user()->id == 1) @endif>
-                                    <a class="nav-link" href="{{ route('user.sponsorships.index') }}">
-                                        Sponsorizzazioni
-                                    </a>
-                                </li>
                                 {{-- <li class="nav-item">
                                     <a class="nav-link" href="{{ route('user.apartments.create') }}">Diventa Host</a>
                                 </li> --}}
                             </ul>
-                        @endif
-
+                            @endif
+                            {{-- @if (Auth::user()->id == 1)
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item" >
+                                    <a class="nav-link" href="{{ route('admin.services.index') }}">
+                                        Servizi
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('user.users.index') }}">
+                                        Utenti
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('user.sponsorships.index') }}">
+                                        Sponsorizzazioni
+                                    </a>
+                                </li>
+                            </ul>
+                            @endif --}}
                         <!-- Right Side Of Navbar -->
                         <ul class="navbar-nav ml-auto">
                             <!-- Authentication Links -->
