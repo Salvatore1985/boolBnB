0032c64eae249de55716894907417d0724cd4f86
FeliciaPompei <f.pompei@hotmail.com>
2022-06-22 15:41:01 +0200

    adding trans. logos

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -67,7 +67,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ url('/') }}">
-            <img id="logo" src="{{asset('images/booBnB-logo.jpg')}}" alt="BoolBnB Logo">
+            <img id="logo" src="{{asset('images/boolBnB-logo.png')}}" alt="BoolBnB Logo">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
