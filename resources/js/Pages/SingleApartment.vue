e4035843adebef9ff50457660454d0cafa9f69b0
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-06-23 20:41:04 +0200

    add style apartmet user single

--- resources/js/Pages/SingleApartment.vue
+++ resources/js/Pages/SingleApartment.vue
@@ -1,138 +1,154 @@
 <template>
-  <div class="container border border-danger">
-    <div :class="alert" class="alert" id="alert-message">
-        {{callResponse}}
+  <div class="container">
+    <div class="row mb-3 justify-content-around">
+      <div
+        class="
+          col-5
+          d-flex
+          align-items-center
+          shadow
+          my-rounded-1 my-bg-card-info
+        "
+      >
+        <section>
+          <div :class="alert" class="alert" id="alert-message">
+            {{ callResponse }}
+          </div>
+          <h1 class="py-4">
+            {{ apartment.title }}
+          </h1>
+          <span>{{ apartment.price }}€ /notte</span>
+          <div class="d-flex avatar">
+            <img
+              class="img-fluid rounded-circle"
+              src="https://i.pinimg.com/474x/4b/71/f8/4b71f8137985eaa992d17a315997791e.jpg"
+              alt=""
+            />
+            <h5>Host: {{ apartment.user.name }}</h5>
+          </div>
+
+          <h6>
+            <span class="font-weight-bold">Sito in via:</span>
+            <span>{{ apartment.address }}</span>
+          </h6>
+        </section>
+      </div>
+      <div
+        class="
+          col-5
+          d-flex
+          align-items-center
+          shadow
+          my-rounded-1 my-bg-card-info
+        "
+      >
+        <!-- service\ -->
+        <div>
+          <h5>Servizi</h5>
+          <ul>
+            <li v-for="(service, index) in services" :key="index">
+              <h5>{{ service.name }}</h5>
+            </li>
+          </ul>
+        </div>
+        <div class="col-12">
+          <div class="">{{ apartment.n_rooms }} camera da letto -</div>
+          <div class="ml-1">{{ apartment.n_beds }} letti -</div>
+          <div class="ml-1">{{ apartment.n_bathrooms }} bagni</div>
+        </div>
+      </div>
     </div>
-    <h1 class="py-4">
-      {{ apartment.title }}
-    </h1>
-    <h6>
-      <span class="font-weight-bold">Sito in via:</span>
-      <span>{{ apartment.address }}</span>
-    </h6>
+    <!-- Img primary -->
     <div class="row">
       <div class="col-6">
-        <img
-          class="my-rounded-1 w-100"
-          :src="apartment.images[0].link"
-          :alt="apartment.tile"
-        />
-      </div>
-      <div class="col-6 border border-danger">Inseriamo il tomtom</div>
-      <div class="row">
-        <div class="col-12"></div>
-      </div>
-      <!-- img secondary -->
-      <!--  <div class="col-6">
-        <div class="row h-100">
-          <div class="col-12 h-100">
-            <div class="row h-100">
-              <div class="col-8 d-flex border-dark">
-                <div class="row">
-                  <div
-                    class="w-100 col-12 d-flex"
-                    v-for="(image, index) in images"
-                    :key="index"
-                  >
-                    <img
-                      v-if="image.link.startsWith('https://')"
-                      class="img-fluid mb-3"
-                      :src="image.link"
-                      :alt="apartment.title"
-                    />
-                    <img
-                      v-else
-                      class="img-fluid mb-3"
-                      :src="`storage/${image.link}`"
-                      :alt="apartment.title"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
- -->
-      <!--  <div class="col-6 d-flex justify-content-between">
         <div>
-          <h3>Host: {{ apartment.user.name }}</h3>
-          <div class="d-flex">
-            <div class="">{{ apartment.n_rooms }} camera da letto -</div>
-            <div class="ml-1">{{ apartment.n_beds }} letti -</div>
-            <div class="ml-1">{{ apartment.n_bathrooms }} bagni</div>
-          </div>
-        </div>
-
-        <div class="d-flex avatar">
           <img
-            class="img-fluid rounded-circle"
-            src="https://i.pinimg.com/474x/4b/71/f8/4b71f8137985eaa992d17a315997791e.jpg"
-            alt=""
+            class="my-rounded-1 w-100"
+            :src="apartment.images[0].link"
+            :alt="apartment.tile"
           />
         </div>
       </div>
- -->
-      <!--   <div class="col-6 d-flex">
-        <span>{{ apartment.price }}€ /notte</span>
-      </div> -->
+      <!-- tomtom -->
+      <div class="col-6 border border-danger">Inseriamo il tomtom</div>
+      <!-- Img secondary -->
+      <div class="row mt-2 shadow">
+        <div
+          class="col-3 g-3 d-flex"
+          v-for="(image, index) in images"
+          :key="index"
+        >
+          <div class="wraper-cover-apartment-small">
+            <img
+              v-if="image.link.startsWith('https://')"
+              class="cover-apartment-small my-rounded-1"
+              :src="image.link"
+              :alt="apartment.title"
+            />
+            <img
+              v-else
+              class="cover-apartment-small my-rounded-1"
+              :src="`storage/${image.link}`"
+              :alt="apartment.title"
+            />
+          </div>
+        </div>
+      </div>
     </div>
-    <!-- service\ -->
-    <div>
-      <ul>
-        <li v-for="(service, index) in services" :key="index">
-          <h5>{{ service.name }}</h5>
-        </li>
-      </ul>
-    </div>
-    <!--   <div class="row">
-      <div class="col-6">
-        <p>
-          Ogni prenotazione include una protezione gratuita in caso di
-          cancellazione da parte dell'host, di inesattezze dell'annuncio e di
-          altri problemi come le difficoltà in fase di check-in.
-        </p>
-      </div>
-    </div> -->
 
-    <!--     <div class="row">
-      <div class="col-6">
-        <h4>Cosa troverai</h4>
-        <div class="w-50">{{ apartment.services[0].name }}</div>
+    <div class="row mt-3">
+      <div class="col-5 d-flex align-items-center">
+        <div>
+          <h4>Info</h4>
+          <div>{{ apartment.description }}</div>
+        </div>
       </div>
-    </div> -->
-
-    <!--
-    <div class="row">
-      <div class="col-12">
-        <h4>Info</h4>
-        <div class="w-50">{{ apartment.description }}</div>
+      <div class="col-6 shadow my-rounded-1">
+        <div>
+          <h3 class="mt-5 h2">Contatta il proprietario</h3>
+          <div class="form-group">
+            <label for="mail">Il tuo nome</label>
+            <input
+              type="email"
+              class="form-control"
+              id="mail"
+              v-model="emailName"
+            />
+          </div>
+          <div class="form-group">
+            <label for="mail">Indirizzo mail</label>
+            <input
+              type="email"
+              class="form-control"
+              id="mail"
+              v-model="email"
+            />
+          </div>
+          <div class="form-group">
+            <label for="emailContent">Example textarea</label>
+            <textarea
+              class="form-control"
+              id="emailContent"
+              rows="5"
+              v-model="emailContent"
+            ></textarea>
+          </div>
+          <button
+            class="btn btn-dark"
+            @click="sendMessage(emailName, email, emailContent)"
+          >
+            Invia
+          </button>
+        </div>
       </div>
-
-      <div class="col-12 text-end my-4">
+      <!--  <div class="col-12 text-end my-4">
         <a
           class="btn btn-sm btn-success"
           href="http://127.0.0.1:8000/user/apartments/create "
         >
           Contatta Host
         </a>
-      </div>
-    </div> -->
-    <div>
-        <h3 class="mt-5 h2">Contatta il proprietario</h3>
-        <div class="form-group">
-            <label for="mail">Il tuo nome</label>
-            <input type="email" class="form-control" id="mail" v-model="emailName">
-            </div>
-            <div class="form-group">
-            <label for="mail">Indirizzo mail</label>
-            <input type="email" class="form-control" id="mail" v-model="email">
-            </div>
-            <div class="form-group">
-            <label for="emailContent">Example textarea</label>
-            <textarea class="form-control" id="emailContent" rows="5" v-model="emailContent"></textarea>
-            </div>
-            <button class="btn btn-dark" @click="sendMessage(emailName,email,emailContent)">Invia</button>
+      </div> -->
     </div>
   </div>
 </template>
@@ -142,14 +158,14 @@ export default {
   name: "SingleApartment",
   data: function () {
     return {
-        apartment: [],
-        images: [],
-        services: [],
-        emailName: '',
-        email: '',
-        emailContent: '',
-        callResponse: '',
-        baseURI : 'http://127.0.0.1:8000/api'
+      apartment: [],
+      images: [],
+      services: [],
+      emailName: "",
+      email: "",
+      emailContent: "",
+      callResponse: "",
+      baseURI: "http://127.0.0.1:8000/api",
     };
   },
   methods: {
@@ -172,15 +188,20 @@ export default {
         });
     },
     sendMessage(emailName, email, emailContent) {
-        if(emailName != '' && email != '' && emailContent != '') {
-            axios.post( `${this.baseURI}/messages/?name=${this.emailName}&email=${this.email}&email_content=${this.emailContent}&apartment_id=${this.apartment.id}`).then(res => {
-                console.log(res);
-                    this.callResponse = "Messaggio inviato con successo";
-            }).catch(() => {
-                        this.callResponse = "Messaggio non inviato";  
-            })
-        }
-    }
+      if (emailName != "" && email != "" && emailContent != "") {
+        axios
+          .post(
+            `${this.baseURI}/messages/?name=${this.emailName}&email=${this.email}&email_content=${this.emailContent}&apartment_id=${this.apartment.id}`
+          )
+          .then((res) => {
+            console.log(res);
+            this.callResponse = "Messaggio inviato con successo";
+          })
+          .catch(() => {
+            this.callResponse = "Messaggio non inviato";
+          });
+      }
+    },
   },
   mounted() {
     console.warn(this.$route.params.id);
