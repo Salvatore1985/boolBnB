9dbd60ad95091008f48c5923b157664e331e7e35
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-06-23 15:31:17 +0200

    add modify header

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,6 +1,88 @@
 <div id="app">
-    <header class="height-header-form shadow">
-        <nav class="navbar navbar-expand-lg navbar-light background_navbar height-header-form h5">
+
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <img id="logo" src="{{ asset('images/boolBnB-logo.png') }}" alt="BoolBnB Logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse flex-grow-0 " id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="{{ route('user.home') }}" id="navbarDropdown"
+                                role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Pannello di controllo
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                @if (Auth::user())
+                                    <li><a class="dropdown-item" href="{{ route('user.apartments.index') }}">I tuoi
+                                            Appartmenti</a></li>
+                                    @if (Auth::id() === 1)
+                                        <li class="nav-item">
+                                            <a class="nav-link" aria-current="page"
+                                                href="{{ route('user.apartments.index') }}">
+                                                I Appartmenti
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" aria-current="page"
+                                                href="{{ route('admin.services.index') }}">
+                                                Servizi
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" aria-current="page"
+                                                href="{{ route('user.users.index') }}">
+                                                Utenti
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" aria-current="page"
+                                                href="{{ route('user.sponsorships.index') }}">
+                                                Sponsorizzazioni
+                                            </a>
+                                        </li>
+                                    @endif
+                                    <hr class="dropdown-divider">
+                                    <li class="nav-item">
+                                        <a class="nav-link" aria-current="page" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                            Log Out
+                                        </a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                            class="d-none">
+                                            @csrf
+                                        </form>
+                                    </li>
+                                @endif
+                                @guest
+                                    <li class="nav-item">
+                                        <a class="nav-link text-black" #aria-current="page" href="{{ route('login') }}">
+                                            {{ __('Login') }}
+                                        </a>
+                                    </li>
+                                    @if (Route::has('register'))
+                                        <li class="nav-item ">
+                                            <a class="nav-link text-black" aria-current="page"
+                                                href="{{ route('register') }}">
+                                                {{ __('Register') }}
+                                            </a>
+                                        </li>
+                                    @endif
+                                @endguest
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        {{-- <nav class="navbar navbar-expand-lg navbar-light background_navbar height-header-form h5">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <img id="logo" src="{{ asset('images/boolBnB-logo.png') }}" alt="BoolBnB Logo">
@@ -78,6 +160,6 @@
                     @endguest
                 </div>
             </div>
-        </nav>
+        </nav> --}}
     </header>
 </div>
