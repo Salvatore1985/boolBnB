9ceae920ddf09f65e6ff92a4bf67ca6d05d17b65
FeliciaPompei <f.pompei@hotmail.com>
2022-06-23 09:58:07 +0200

    debugged

--- resources/js/components/Apartment.vue
+++ resources/js/components/Apartment.vue
@@ -1,100 +1,55 @@
 <template>
-  <div class="col-md-6 col-lg-4 col-sm-12 justify-content-between py-4">
-    <section class="my-rounded-1 p-3">
-      <!-- Image Apartment -->
-      <div
-        class="text-center mb-3"
-        v-if="apartment.images[0].link.startsWith('https://')"
-      >
+    <div class="col-md-6 col-lg-4 col-sm-12 justify-content-between py-4">
+        <section class="my-rounded-1 p-3">
+            <!-- Image Apartment -->
+            <div
+                class="text-center mb-3"
+                v-if="apartment.images[0].link.startsWith('https://')"
+            >
                 <img
-                class="img-apartment"
+                class="img-apartment my-rounded-1"
                 :src="apartment.images[0].link"
-                :alt="apartment.title">
-            </div>
-            <div class=" bg-dark text-center mb-3"
-            v-else>
-                <img
-                class="img-apartment"
-                :src="`storage/${apartment.images[0].link}`"
-                :alt="apartment.title">
-            </div>
-        <h6 class="py-2">stelline</h6>
-        <h5 class="card-title">{{ apartment.title }}</h5>
-        <p class="card-text">
-            {{ limitOverview(apartment) }}
-        </p>
-        <pre>Creato il: {{ getFormattedDate(apartment.created_at) }}</pre>
-        <div class="d-flex justify-content-between">
-            <div class="d-flex avatar">
-                <img
-                class="img-fluid rounded-circle"
-                src="https://i.pinimg.com/474x/4b/71/f8/4b71f8137985eaa992d17a315997791e.jpg"
-                alt=""
+                :alt="apartment.title"
                 />
-                <span class="px-3">Nome host</span>
-                <!-- <a href="/contact">Contatta l'inserzionista</a> -->
+            </div>
+            <div class="text-center mb-3" v-else>
+                <img
+                class="img-apartment my-rounded-1"
+                :src="`storage/${apartment.images[0].link}`"
+                :alt="apartment.title"
+                />
+            </div>
+            <!-- info Apartment -->
+            <h6 class="py-2">
+                stelline
+            </h6>
+            <h5 class="card-title">
+                {{ apartment.title }}
+            </h5>
+            <p class="card-text">
+                {{ limitOverview(apartment) }}
+            </p>
+            <pre>Creato il: {{ getFormattedDate(apartment.created_at) }}</pre>
+            <div class="d-flex justify-content-between">
+                <div class="d-flex avatar">
+                <img
+                    class="img-fluid rounded-circle"
+                    src="https://i.pinimg.com/474x/4b/71/f8/4b71f8137985eaa992d17a315997791e.jpg"
+                    alt=""
+                />
+                <span class="px-3">
+                    {{ apartment.user.name }}
+                </span>
+                </div>
             </div>
             <h5 class="px-3">{{ apartment.price }} €/Notte</h5>
-        </div>
-    <div class="bg-dark text-center mb-3">
-      <img
-        class="img-apartment"
-        :src="`storage/${apartment.images[0].link}`"
-        :alt="apartment.title"
-      />
+            <div>
+                <router-link :to="{ name: 'apartment', params: { id: apartment.id } }">
+                    Leggi questo post
+                </router-link>
+            </div>
+        </section>
     </div>
-    <h6 class="py-2">stelline</h6>
-    <h5 class="card-title">{{ apartment.title }}</h5>
-    <p class="card-title">{{ apartment.address }}</p>
-    <p class="card-text">
-        <!-- <span class="px-3">
-            {{apartment.service.name}}
-        </span> -->
-      {{ limitOverview(apartment) }}
-    </p>
-    <pre>Creato il: {{ getFormattedDate(apartment.created_at) }}</pre>
-    <div class="d-flex justify-content-between">
-      <div class="d-flex avatar">
-        <img
-          class="img-apartment my-rounded-1"
-          :src="apartment.images[0].link"
-          :alt="apartment.title"
-        />
-      </div>
-      <div class="text-center mb-3" v-else>
-        <img
-          class="img-apartment my-rounded-1"
-          :src="`storage/${apartment.images[0].link}`"
-          :alt="apartment.title"
-        />
-      </div>
-      <!-- info Apartment -->
-      <h6 class="py-2">stelline</h6>
-      <h5 class="card-title">{{ apartment.title }}</h5>
-      <p class="card-text">
-        {{ limitOverview(apartment) }}
-      </p>
-      <pre>Creato il: {{ getFormattedDate(apartment.created_at) }}</pre>
-      <div class="d-flex justify-content-between">
-        <div class="d-flex avatar">
-          <img
-            class="img-fluid rounded-circle"
-            src="https://i.pinimg.com/474x/4b/71/f8/4b71f8137985eaa992d17a315997791e.jpg"
-            alt=""
-          />
-          <span class="px-3">
-            {{ apartment.user.name }}
-          </span>
-        </div>
-        <h5 class="px-3">{{ apartment.price }} €/Notte</h5>
-      </div>
-      <div>
-        <router-link :to="{ name: 'apartment', params: { id: apartment.id } }">
-          Leggi questo post
-        </router-link>
-      </div>
-    </section>
-  </div>
 </template>
 
 <script>
