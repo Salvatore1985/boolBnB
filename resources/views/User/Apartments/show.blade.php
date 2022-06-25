bec6d8b324855efc4d49b5b2f6f6353706b15f8e
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-06-25 11:20:47 +0200

    show admin

--- resources/views/User/Apartments/show.blade.php
+++ resources/views/User/Apartments/show.blade.php
@@ -1,13 +1,12 @@
 <style>
     #map-div {
-        width: 30vw;
-        height: 50vh;
+        width: 500px;
+        height: 450px;
     }
 </style>
 @extends('layouts.createPage')
 
 @section('form-content')
-
     <div class="container py-5">
         <div class="row">
             <div class="col-12">
@@ -17,137 +16,111 @@
                     </div>
                 @endif
             </div>
-            <div class="col-6 d-flex">
+            <div class="col-12 col-lg-8 d-flex my-bg-card-map">
                 <div>
                     <h1>{{ $apartment->title }}</h1>
-                    <h2>{{ $apartment->address }}</h2>
+                    <h2>Sito in: {{ $apartment->address }}</h2>
                 </div>
-
             </div>
-            <div class="col-6 d-flex flex-column align-items-end">
-                <div class="py-2">
-
-                    <a href="{{ route('user.apartments.index') }} ">
-                        Torna all'appartamenti
-                    </a>
-                </div>
-                <div class="py-2">
-
-                    <a href="{{ route('user.apartments.edit', $apartment) }} "class="btn btn-warning">
-                        &#9998; Modifica
-                    </a>
-                </div>
-                {{-- delete form --}}
-                <div>
-
-                    <form action="{{ route('user.apartments.destroy', $apartment) }}" method="POST"
-                        class="apartment-destroyer" apartment-name="{{ ucfirst($apartment->title) }}"
-                        onclick="return confirm('Sei sicuro di voler eliminare {{ $apartment->title }}?')">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-md btn-delete btn-outline-danger" type="submit">
-                            &#10008; Elimina
-                        </button>
-                    </form>
-                </div>
-                <a href="{{ route('user.sponsorships.purchase', $apartment) }} "class="btn btn-primary">
-                    Sponsorizza
-                </a>
+            {{-- form --}}
+            <div class="col-12 col-lg-4 my-5">
+                <ul class="d-flex justify-content-between align-items-center">
+                    <li class="text-decoration-none list-style-type-none">
+                        <a href="{{ route('user.apartments.index') }} "class="btn btn-success ">
+                            &#8630;
+                        </a>
+                    </li>
+                    <li class="text-decoration-none list-style-type-none">
+                        <a href="{{ route('user.apartments.edit', $apartment) }} "class="btn btn-warning">
+                            &#9998;
+                        </a>
+                    </li>
+                    <li class="text-decoration-none list-style-type-none d-flex align-items-center">
+                        <form action="{{ route('user.apartments.destroy', $apartment) }}" method="POST"
+                            class="apartment-destroyer mx-0 my-auto " apartment-name="{{ ucfirst($apartment->title) }}"
+                            onclick="return confirm('Sei sicuro di voler eliminare {{ $apartment->title }}?')">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-md btn-delete btn-outline-danger" type="submit">
+                                &#10008;
+                            </button>
+                        </form>
+                    </li>
+                    <li class="text-decoration-none list-style-type-none">
+                        <a href="{{ route('user.sponsorships.purchase', $apartment) }} "class="btn btn-primary">
+                            Sponsorizza
+                        </a>
+                    </li>
+                </ul>
             </div>
-            <div class="col-6 ">
+        </div>
+        <div class="row justify-content-between">
+            {{-- image --}}
+            <div class="col-12 col-lg-6 my-5 my-bg-card-map ">
                 @foreach ($apartment->images as $image)
                     @if (str_starts_with($image->link, 'https://') || str_starts_with($image->link, 'http://'))
-                        <img class="rounded-1 w-100 " src="{{ $image->link }}" alt="{{ $apartment->title }}">
+                        <img class=" w-100 p-3" src="{{ $image->link }}" alt="{{ $apartment->title }}">
                     @else
-                        <img class="rounded-1 w-100 " src="{{ asset('/storage') . '/' . $image->link }}"
+                        <img class=" w-100 p-3" src="{{ asset('/storage') . '/' . $image->link }}"
                             alt="{{ $apartment->title }}">
                     @endif
                 @endforeach
             </div>
-            <div class="col-6">
-                <div id="map-div" lat="{{ $apartment->lat }}" log="{{ $apartment->long }}"></div>
-                <div>
-                    <h1>{{ $apartment->description }}</h1>
-
-                    <ul class="list-group">
-                        <li class="list-group-item">Metri quadri: {{ $apartment->sqr_meters }}</li>
-                        <li class="list-group-item">N. Camere: {{ $apartment->n_rooms }}</li>
-                        <li class="list-group-item">N. Bagni: {{ $apartment->n_bathrooms }}</li>
-                        <li class="list-group-item">N. Letti: {{ $apartment->n_beds }}</li>
-                        <li class="list-group-item">N. Price: {{ $apartment->price }}</li>
-                    </ul>
-                    <ul class="list-group">
-                        @if (count($apartment->services) !== 0)
-                            <li class="list-group-item">
-                                servizi:
-                                @foreach ($apartment->services as $service)
-                                    <pre class="me-2">{{ $service->name }}</pre>
-                                @endforeach
-                            </li>
-                        @else
-                            Non ci sono servizi
-                        @endif
-                    </ul>
-                    <div>
-                        @forelse ($apartment->messages as $message)
-                            <div class="card-body border-bottom">
-                                <h5 class="card-title h6"><span class="font-italic">E-mail:</span> {{ $message->email }}
-                                </h5>
-                                <p class="card-text"><span class="font-italic">Messaggio:</span>
-                                    {{ $message->email_content }}</p>
-                            </div>
-                        @empty
-                            <div class="card-body border-bottom">Non ci sono messaggi</div>
-                        @endforelse
+            {{-- tomtom --}}
+            <div class="col-12 col-lg-5 my-5  ">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="my-bg-card-map" id="map-div" lat="{{ $apartment->lat }}"
+                            log="{{ $apartment->long }}">
+                        </div>
                     </div>
+                    {{-- service --}}
+                    <div class="col-12 my-3">
+                        <div>
+                            <h1>Descrizione</h1>
+                            <p>{{ $apartment->description }}</p>
 
+                            <ul class="list-group">
+                                <li class="list-group-item">Metri quadri: {{ $apartment->sqr_meters }}</li>
+                                <li class="list-group-item">N. Camere: {{ $apartment->n_rooms }}</li>
+                                <li class="list-group-item">N. Bagni: {{ $apartment->n_bathrooms }}</li>
+                                <li class="list-group-item">N. Letti: {{ $apartment->n_beds }}</li>
+                                <li class="list-group-item">N. Price: {{ $apartment->price }}</li>
+                            </ul>
+                            <ul class="list-group">
+                                @if (count($apartment->services) !== 0)
+                                    <li class="list-group-item">
+                                        servizi:
+                                        @foreach ($apartment->services as $service)
+                                            <pre class="me-2">{{ $service->name }}</pre>
+                                        @endforeach
+                                    </li>
+                                @else
+                                    Non ci sono servizi
+                                @endif
+                            </ul>
+                            <div>
+                                @forelse ($apartment->messages as $message)
+                                    <div class="card-body border-bottom">
+                                        <h5 class="card-title h6"><span class="font-italic">E-mail:</span>
+                                            {{ $message->email }}
+                                        </h5>
+                                        <p class="card-text"><span class="font-italic">Messaggio:</span>
+                                            {{ $message->email_content }}</p>
+                                    </div>
+                                @empty
+                                    <div class="card-body border-bottom">Non ci sono messaggi</div>
+                                @endforelse
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
-
 
             </div>
-
-            {{-- <section class="d-flex ">
-                @foreach ($apartment->images as $image)
-                    <div class="col-6 show-img mb-3">
-                        @if (str_starts_with($image->link, 'https://') || str_starts_with($image->link, 'http://'))
-                            <img class="rounded-1 w-100" src="{{ $image->link }}" alt="{{ $apartment->title }}">
-                        @else
-                            <img class="rounded-1 w-100" src="{{ asset('/storage') . '/' . $image->link }}"
-                                alt="{{ $apartment->title }}">
-                        @endif
-                    </div>
-                @endforeach
-                <div>
-                    <p>{{ $apartment->description }}</p>
-
-                    <ul class="list-group">
-                        <li class="list-group-item">Metri quadri: {{ $apartment->sqr_meters }}</li>
-                        <li class="list-group-item">N. Camere: {{ $apartment->n_rooms }}</li>
-                        <li class="list-group-item">N. Piani: {{ $apartment->n_floor }}</li>
-                        <li class="list-group-item">N. Bagni: {{ $apartment->n_bathrooms }}</li>
-                        <li class="list-group-item">N. Letti: {{ $apartment->n_beds }}</li>
-                        <li class="list-group-item">N. Price: {{ $apartment->price }}</li>
-                    </ul>
-                    <ul class="list-group">
-                        @if (count($apartment->services) !== 0)
-                            <li class="list-group-item">
-                                servizi:
-                                @foreach ($apartment->services as $service)
-                                    <pre class="me-2">{{ $service->name }}</pre>
-                                @endforeach
-                            </li>
-                        @else
-                            Non ci sono servizi
-                        @endif
-                    </ul>
-
-                </div>
-            </section> --}}
-
-            {{-- <div class="col-5">
-                <div id="map-div" lat="{{ $apartment->lat }}" log="{{ $apartment->long }}"></div>
-            </div> --}}
         </div>
+
+
     </div>
 @endsection
 
