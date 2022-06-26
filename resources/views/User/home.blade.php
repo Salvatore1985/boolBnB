bd9749835d4ce4d1151d1a6600b9d4b7d84a8d56
FeliciaPompei <f.pompei@hotmail.com>
2022-06-26 16:41:43 +0200

    WIP

--- resources/views/User/home.blade.php
+++ resources/views/User/home.blade.php
@@ -2,59 +2,117 @@
 @section('title', 'Dashboard')
 @section('content')
 <div class="container mt-2 mb-2">
-    <div class="row justify-content-center ">
+    <div class="row justify-content-center mb-4">
         <div class="col-12">
             <div class="card custom-container">
                 <div class="card-header">
                     <h1>
                         Ciao {{ Auth::user()->name }} !
                     </h1>
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <h4>Login effettuato con successo!</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center mb-4">
+        <div class="col-12">
+            <div class="card custom-container">
+                <div class="card-header">
+                    {{-- Delete User Message --}}
+                    <div class="col-12 my-container container ">
+                        @if (session('alert-message'))
+                        <div class="alert alert-{{ session('alert-type') }}">
+                        {{ session('alert-message') }}
+                        </div>
                         @endif
-                        <h4>Login effettuato con successo!</h4>
-                        <h6>Messaggi:</h6>
+                    </div>
+                    <div class="col-12 m-3">
+                        <h1 class="mb-3">I tuoi messaggi:</h1>
+
+                    @forelse ($apartments  as $apartment)
+                    @foreach ($apartment->messages as $message)
+                    <div class="card chat " style="width: 18rem;">
+                        <div class="card-body ">
+                            <h5 class="card-title">Da: {{$message->email}}</h5>
+                                <div ><p class="card-text my-scroll">{{$message->email_content}} </p></div>
+                                    <p class="card-text">{{$message->created_at}}</p>
+                                    <a href="" class="btn btn-outline-primary">
+                                        reply
+                                    </a>
+                                    <form action="{{ route('user.message.destroy', $message->id) }}" method="POST"
+                                        class="messagge-destroyer" message-email_content="{{ ucfirst($message->email_content) }}"
+                                            onclick="return confirm('Sei sicuro di voler eliminare il messaggio di {{ $message->name }}?')">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="btn btn-pink btn-md btn-delete btn-outline-danger" type="submit">
+                                            &#10008;
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    @endforeach
+                        @empty
+                            <div class="card-body border-bottom">
+                                <p>
+                                    Non ci sono messaggi
+                                </p>
+                            </div>
+                        @endforelse
                     </div>
                 </div>
             </div>
         </div>
     </div>
-                        {{-- Delete User Message --}}
-    <div class="col-12 my-container container ">
-    @if (session('alert-message'))
-        <div class="alert alert-{{ session('alert-type') }}">
-                {{ session('alert-message') }}
+    {{-- <div class="row justify-content-center">
+        {{-- Delete User Message
+        <div class="col-12 my-container container ">
+            @if (session('alert-message'))
+            <div class="alert alert-{{ session('alert-type') }}">
+            {{ session('alert-message') }}
+            </div>
+            @endif
         </div>
-    @endif
-    <div class="row">
+        <div class="col-12 custom-container m-3">
+            <h1>I tuoi messaggi:</h1>
+        </div>
         @forelse ($apartments  as $apartment)
-                @foreach ($apartment->messages as $message)
-                    <div class="col-6 d-flex justify-content-center box">
-                        <div class="card chat " style="width: 18rem;">
-                            <div class="card-body ">
-                                <h5 class="card-title">Da: {{$message->email}}</h5>
-                                    <div ><p class="card-text my-scroll">{{$message->email_content}} </p></div>
-                                        <p class="card-text">{{$message->created_at}}</p>
-                                        <form action="{{ route('user.message.destroy', $message->id) }}" method="POST"
-                                            class="messagge-destroyer" message-email_content="{{ ucfirst($message->email_content) }}"
-                                                onclick="return confirm('Sei sicuro di voler eliminare il messaggio di {{ $message->name }}?')">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button class="btn btn-pink btn-md btn-delete btn-outline-danger" type="submit">
-                                                &#10008; Elimina
-                                            </button>
-                                        </form>
-                                    </div>
-                                </div>
+            @foreach ($apartment->messages as $message)
+                <div class="col-6 d-flex justify-content-center box">
+                    <div class="card chat " style="width: 18rem;">
+                        <div class="card-body ">
+                            <h5 class="card-title">Da: {{$message->email}}</h5>
+                                <div ><p class="card-text my-scroll">{{$message->email_content}} </p></div>
+                                    <p class="card-text">{{$message->created_at}}</p>
+                                    <a href="" class="btn btn-outline-primary">
+                                        reply
+                                    </a>
+                                    <form action="{{ route('user.message.destroy', $message->id) }}" method="POST"
+                                        class="messagge-destroyer" message-email_content="{{ ucfirst($message->email_content) }}"
+                                            onclick="return confirm('Sei sicuro di voler eliminare il messaggio di {{ $message->name }}?')">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="btn btn-pink btn-md btn-delete btn-outline-danger" type="submit">
+                                            &#10008;
+                                    </button>
+                                </form>
                             </div>
                         </div>
                     </div>
-                @endforeach
-            @empty
-        <div class="card-body border-bottom">Non ci sono messaggi</div>
-    @endforelse
+            @endforeach
+        @empty
+            <div class="card-body border-bottom">
+                <p>
+                    Non ci sono messaggi
+                </p>
+            </div>
+        @endforelse
+    </div> --}}
+</div>
 @endsection
 
 @section('js-files')
