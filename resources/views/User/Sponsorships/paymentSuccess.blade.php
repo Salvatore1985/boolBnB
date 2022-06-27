a7aaa407122e5c6011ccde08040a6dfeeb694465
FeliciaPompei <f.pompei@hotmail.com>
2022-06-27 17:05:56 +0200

    feat. added thank you page

---
+++ resources/views/User/Sponsorships/paymentSuccess.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('title', 'Thank you')
+@section('content')
+    <div class="container my-bg-card-map">
+        <div class="row">
+            <div class="col-12 p-5 text-center">
+                <h1>
+                    Grazie mille per l'acquisto
+                    <br>
+                    Buon proseguimento!
+                </h1>
+            </div>
+        </div>
+    </div>
+@endsection
